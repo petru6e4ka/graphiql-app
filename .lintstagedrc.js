16050caf62fdcafbe0f9493ba51385f6716df622
anastasiiavorobiova <anastasiiavorobiova2404@gmail.com>
2024-08-20 23:40:17 +0300

    fix: husky

---
+++ .lintstagedrc.js
@@ -0,0 +1,9 @@
+const path = require('path');
+
+const buildPrettierCommand = (filenames) => `prettier --write ${filenames.map((f) => path.relative(process.cwd(), f)).join(' --file ')}`;
+const buildEslintCommand = (filenames) => `next lint --fix --file ${filenames.map((f) => path.relative(process.cwd(), f)).join(' --file ')}`;
+
+module.exports = {
+  '*.{js,ts,tsx,jsx,css}': [buildPrettierCommand],
+  '*.{js,jsx,ts,tsx}': [buildEslintCommand],
+};

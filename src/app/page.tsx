5e44b48670295e3243da7279a72a3d10ddd9565f
anastasiiavorobiova <anastasiiavorobiova2404@gmail.com>
2024-08-20 19:12:05 +0300

    fix: scripts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -57,19 +57,6 @@ export default function Home() {
           </h2>
           <p>Explore starter templates for Next.js.</p>
         </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy
-            <span>-&gt;</span>
-          </h2>
-          <p>Instantly deploy your Next.js site to a shareable URL with Vercel.</p>
-        </a>
       </div>
     </main>
   );

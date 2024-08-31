07fe6960cc722b4fc86f6b5bd4392cff4467374c
zytsev <zaes2000@mail.ru>
2024-08-31 16:39:50 +0100

    feat: save headers in state

--- src/shared/ui/TwoInputs/TwoInputs.tsx
+++ src/shared/ui/TwoInputs/TwoInputs.tsx
@@ -1,12 +1,19 @@
-import React from 'react';
+import { useState } from 'react';
 import styles from './TwoInputs.module.css';
 import { TextInput } from '../index';
 
 export function TwoInputs() {
+  const [state, setState] = useState({ Key: '', Value: '' });
+
+  const handleInputChange = (name: string) => (event: React.FormEvent<HTMLInputElement>) => {
+    const target = event.target as HTMLInputElement;
+    setState({ ...state, [name]: target.value });
+  };
+
   return (
     <div className={styles.container}>
-      <TextInput className={styles.textinput} placeholder="Key" />
-      <TextInput className={styles.textinput} placeholder="Value" />
+      <TextInput onChange={handleInputChange('Key')} value={state.Key} name="Key" className={styles.textinput} placeholder="Key" />
+      <TextInput onChange={handleInputChange('Value')} value={state.Value} name="Value" className={styles.textinput} placeholder="Value" />
     </div>
   );
 }

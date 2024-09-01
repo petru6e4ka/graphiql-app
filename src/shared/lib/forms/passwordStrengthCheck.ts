cd22b46d3af9922667ced03676d68e6e8f2cac9f
anastasiiavorobiova <anastasiiavorobiova2404@gmail.com>
2024-09-01 23:26:33 +0300

    fix: added regexp to variables with meaningful names

--- src/shared/lib/forms/passwordStrengthCheck.ts
+++ src/shared/lib/forms/passwordStrengthCheck.ts
@@ -1,17 +1,9 @@
+const containsDigitRegex = /[0-9]/;
+const containsLetterRegex = /[A-Za-z\u0400-\u04FF]/;
+const containsSpecialCharRegex = /[$&+,:;=?@#|'<>.^*()%!-]/;
+
 export const passwordStrengthCheck = (invalidMessage: string) => (value: string) => {
-  if (value.length < 8) {
-    return invalidMessage;
-  }
-
-  if (!/[0-9]/.test(value)) {
-    return invalidMessage;
-  }
-
-  if (!/[A-Za-z\u0400-\u04FF]/.test(value)) {
-    return invalidMessage;
-  }
-
-  if (!/[$&+,:;=?@#|'<>.^*()%!-]/.test(value)) {
+  if (value.length < 8 || !containsDigitRegex.test(value) || !containsLetterRegex.test(value) || !containsSpecialCharRegex.test(value)) {
     return invalidMessage;
   }
 

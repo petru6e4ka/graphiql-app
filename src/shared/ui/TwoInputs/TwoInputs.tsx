81d1ae77b563d6e36d81fe6b4e25f7d29be9c8d6
zytsev <zaes2000@mail.ru>
2024-09-04 09:22:16 +0100

    feat: add save headers in store zustand

--- src/shared/ui/TwoInputs/TwoInputs.tsx
+++ src/shared/ui/TwoInputs/TwoInputs.tsx
@@ -1,9 +1,15 @@
 import { useState } from 'react';
+import { useHeaders } from '@/features/store/store';
 import styles from './TwoInputs.module.css';
 import { TextInput } from '../index';
 
-export function TwoInputs() {
-  const [state, setState] = useState({ Key: '', Value: '' });
+interface PropsTwoInputs {
+  id: string;
+}
+
+export function TwoInputs({ id }: PropsTwoInputs) {
+  const [state, setState] = useState({ id, Key: '', Value: '' });
+  const { updateHeaderInStore } = useHeaders();
 
   const handleInputChange = (name: string) => (event: React.FormEvent<HTMLInputElement>) => {
     const target = event.target as HTMLInputElement;
@@ -12,8 +18,22 @@ export function TwoInputs() {
 
   return (
     <div className={styles.container}>
-      <TextInput onChange={handleInputChange('Key')} value={state.Key} name="Key" className={styles.textinput} placeholder="Key" />
-      <TextInput onChange={handleInputChange('Value')} value={state.Value} name="Value" className={styles.textinput} placeholder="Value" />
+      <TextInput
+        onChange={handleInputChange('Key')}
+        onBlur={() => updateHeaderInStore(state)}
+        value={state.Key}
+        name="Key"
+        className={styles.textinput}
+        placeholder="Key"
+      />
+      <TextInput
+        onChange={handleInputChange('Value')}
+        onBlur={() => updateHeaderInStore(state)}
+        value={state.Value}
+        name="Value"
+        className={styles.textinput}
+        placeholder="Value"
+      />
     </div>
   );
 }
--- src/widgets/HeadersSection/ui/HeadersSection.tsx
+++ src/widgets/HeadersSection/ui/HeadersSection.tsx
@@ -2,46 +2,44 @@
 
 import { useState } from 'react';
 import Image from 'next/image';
+import { nanoid } from 'nanoid';
+import { useHeaders } from '@/features/store/store';
 import { TwoInputs } from '@/shared/ui/TwoInputs/TwoInputs';
 import IconPlus from '@/shared/assets/icons/plus-circle.svg';
 import IconClose from '@/shared/assets/icons/x-circle.svg';
 import styles from './HeadersSection.module.css';
 
 export function HeadersSection() {
-  const [countHeaders, setCountHeaders] = useState(0);
-  const arrHeaders = [...Array(countHeaders).keys()].map((i) => i + 1);
+  const [HeadersId, setHeadersId] = useState<string[]>([]);
+  const { addHeaderInStore, removeHeaderFromStore } = useHeaders();
 
-  function onChangeCountHeaders(value: string) {
-    switch (value) {
-      case '+':
-        setCountHeaders(countHeaders + 1);
-        break;
-      case '-':
-        if (countHeaders > 0) {
-          setCountHeaders(countHeaders - 1);
-        }
-        break;
-      default:
-        break;
-    }
-  }
+  const createHeader = () => {
+    const id = nanoid();
+    setHeadersId([...HeadersId, id]);
+    addHeaderInStore({ id, Key: '', Value: '' });
+  };
+
+  const removeHeader = (id: string) => {
+    setHeadersId(HeadersId.filter((elem) => elem !== id));
+    removeHeaderFromStore(id);
+  };
 
   return (
     <div className={styles.container}>
       <div className={styles.titles}>
         <h3 className={styles.h3}>Headers</h3>
-        <Image onClick={() => onChangeCountHeaders('+')} className={styles.IconPlus} src={IconPlus} alt="Plus" width={25} height={25} />
+        <Image onClick={createHeader} className={styles.IconPlus} src={IconPlus} alt="Plus" width={25} height={25} />
       </div>
-      {arrHeaders.length > 0 && (
+      {HeadersId.length > 0 && (
         <div className={styles.spanwrap}>
           <span className={styles.span}>Key</span>
           <span className={styles.span}>Value</span>
         </div>
       )}
-      {arrHeaders.map((elem) => (
-        <div key={elem} className={styles.inputswrap}>
-          <TwoInputs />
-          <Image onClick={() => onChangeCountHeaders('-')} className={styles.IconClose} src={IconClose} alt="Close" width={25} height={25} />
+      {HeadersId.map((id) => (
+        <div key={id} className={styles.inputswrap}>
+          <TwoInputs id={id} />
+          <Image onClick={() => removeHeader(id)} className={styles.IconClose} src={IconClose} alt="Close" width={25} height={25} />
         </div>
       ))}
     </div>

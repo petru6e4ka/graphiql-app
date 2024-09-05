25884ec34cbffc2c475847e3a625ce1c482c13f1
anastasiiavorobiova <anastasiiavorobiova2404@gmail.com>
2024-09-05 10:44:43 +0300

    feat: inputs for rest

--- src/app/[locale]/(private)/rest/page.tsx
+++ src/app/[locale]/(private)/rest/page.tsx
@@ -1,5 +1,7 @@
 import { useTranslations } from 'next-intl';
-import { Title, Button } from '@/shared/ui';
+import {
+  Title, Button, Select, TextInput, Group, JsonInput, Stack,
+} from '@/shared/ui';
 
 import styles from './Rest.module.css';
 
@@ -9,7 +11,32 @@ export default function Rest() {
   return (
     <div className={styles.rest}>
       <Title>{t('title')}</Title>
-      <Button>{t('body')}</Button>
+
+      <div className={styles.content}>
+        <Title order={2}>Request data</Title>
+
+        <Stack>
+          <Group>
+            <div className={styles.method}>
+              <Select
+                placeholder="Method"
+                data={[
+                  { value: 'GET', label: 'GET' },
+                  { value: 'POST', label: 'POST' },
+                  { value: 'DELETE', label: 'DELETE' },
+                  { value: 'PUT', label: 'PUT' },
+                  { value: 'PATCH', label: 'PATCH' },
+                ]}
+              />
+            </div>
+            <div className={styles.url}>
+              <TextInput placeholder="URL" />
+            </div>
+          </Group>
+          <JsonInput placeholder="Body" validationError="Invalid json" formatOnBlur autosize minRows={6} />
+          <Button>Send</Button>
+        </Stack>
+      </div>
     </div>
   );
 }
--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -1,5 +1,21 @@
 export {
-  Title, Text, Group, Burger, Drawer, ScrollArea, Divider, rem, TextInput, PasswordInput, Box, Center, Progress, Anchor,
+  Select,
+  Title,
+  Text,
+  Group,
+  Burger,
+  Drawer,
+  ScrollArea,
+  Divider,
+  rem,
+  TextInput,
+  PasswordInput,
+  Box,
+  Center,
+  Progress,
+  Anchor,
+  JsonInput,
+  Stack,
 } from '@mantine/core';
 export { Logo } from './Logo';
 export { Button } from './Button';

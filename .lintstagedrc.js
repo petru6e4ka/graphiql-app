cbb35aa8d77ab29d2a1df065c7ddb8e3fa97eeb9
Anastasiia Vorobiova <51080393+petru6e4ka@users.noreply.github.com>
2024-09-15 15:53:51 +0300

    fix:  Delete .lintstagedrc.js

--- .lintstagedrc.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-const buildPrettierCommand = (filenames) => `prettier --write ${filenames.map((f) => path.relative(process.cwd(), f)).join(' --file ')}`;
-const buildEslintCommand = (filenames) => `next lint --fix --file ${filenames.map((f) => path.relative(process.cwd(), f)).join(' --file ')}`;
-
-module.exports = {
-  '*.{js,ts,tsx,jsx,css}': [buildPrettierCommand],
-  '*.{js,jsx,ts,tsx}': [buildEslintCommand],
-};
